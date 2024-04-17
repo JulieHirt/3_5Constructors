141ef6c68e2ff8add48fcab782adede27dc2ff41
JulieBodette <jr3python@gmail.com>
2024-04-17 10:53:10 -0400

    Update DateTimePickerWindow.cs

--- Constructors3_5/DateTimePickerWindow.cs
+++ Constructors3_5/DateTimePickerWindow.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using Wisej.Web;
 
 namespace Constructors3_5
@@ -13,6 +14,22 @@ namespace Constructors3_5
 		private void DateTimePickerWindow_Load(object sender, EventArgs e)
 		{
 			DateTimePicker datetimepicker1 = new DateTimePicker();
+			this.Controls.Add(datetimepicker1);
+
+			Action<object, EventArgs> action = (obj, args) => { AlertBox.Show("changed"); };
+			DateTimePicker datetimepicker2 = new DateTimePicker(action);
+			datetimepicker2.Location = new Point(300, 50);
+			this.Controls.Add(datetimepicker2);
+
+			DateTimePicker datetimepicker3 = new DateTimePicker("label", action);
+			datetimepicker3.Location = new Point(200, 50);
+			this.Controls.Add(datetimepicker3);
+
+			Point p = new Point(400, 50);
+			Size s = new Size(100, 100);
+			DateTimePicker datetimepicker4 = new DateTimePicker(p, s);
+			this.Controls.Add(datetimepicker4);
+
 		}
 	}
 }
