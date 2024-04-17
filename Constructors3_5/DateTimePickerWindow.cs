4969946368cf7dc7ac757aeeafc58b8e04f6c04e
JulieBodette <jr3python@gmail.com>
2024-04-17 14:47:08 -0400

    Update DateTimePickerWindow.cs

--- Constructors3_5/DateTimePickerWindow.cs
+++ Constructors3_5/DateTimePickerWindow.cs
@@ -15,6 +15,7 @@ namespace Constructors3_5
 		{
 			DateTimePicker datetimepicker1 = new DateTimePicker();
 			this.Controls.Add(datetimepicker1);
+			AlertBox.Show(datetimepicker1.Size.ToString());
 
 			Action<object, EventArgs> action = (obj, args) => { AlertBox.Show("changed"); };
 			DateTimePicker datetimepicker2 = new DateTimePicker(action);
@@ -25,10 +26,21 @@ namespace Constructors3_5
 			datetimepicker3.Location = new Point(200, 50);
 			this.Controls.Add(datetimepicker3);
 
-			Point p = new Point(400, 50);
-			Size s = new Size(100, 100);
+			Point p = new Point(200, 100);
+			Size s = new Size(200, 30);
 			DateTimePicker datetimepicker4 = new DateTimePicker(p, s);
 			this.Controls.Add(datetimepicker4);
+
+			DateTimePicker datetimepicker5 = new DateTimePicker(p, s, action);
+			datetimepicker5.Location = new Point(300, 100);
+			this.Controls.Add(datetimepicker5);
+
+
+			Point p1 = new Point(200, 200);
+			Size s1 = new Size(200, 30);
+			Action<object, EventArgs> action1 = (obj, args) => { AlertBox.Show("changed1"); };
+			DateTimePicker datetimepicker6 = new DateTimePicker("label", p1, s1, action1);
+			this.Controls.Add(datetimepicker6);
 
 		}
 	}
