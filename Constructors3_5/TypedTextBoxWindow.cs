3ffcfb11985b7b13848e6e33e49a1ec51ff93fab
JulieBodette <jr3python@gmail.com>
2024-06-11 13:58:06 -0400

    Update TypedTextBoxWindow.cs

--- Constructors3_5/TypedTextBoxWindow.cs
+++ Constructors3_5/TypedTextBoxWindow.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Security.Policy;
 using Wisej.Web;
 
 namespace Constructors3_5
@@ -31,6 +32,28 @@ namespace Constructors3_5
 			TypedTextBox typedTextBox3 = new TypedTextBox("label",action);
 			typedTextBox3.Location = new Point(300, 0);
 			this.Controls.Add(typedTextBox3);
+
+			//constructor 4
+			Point location4 = new Point(0, 100);
+			Size size4 = new Size(200, 100);
+			TypedTextBox typedTextBox4 = new TypedTextBox(location4, size4);
+			this.Controls.Add(typedTextBox4);
+
+			Point location5 = new Point(300, 100);
+			Size size5 = new Size(200, 100);
+			TypedTextBox typedTextBox5 = new TypedTextBox(location5, size5, action);
+			this.Controls.Add(typedTextBox5);
+
+			//constructor 5
+			Point location6 = new Point(0, 200);
+			Size size6 = new Size(200, 100);
+			TypedTextBox typedTextBox6 = new TypedTextBox("label", location6, size6);
+			this.Controls.Add(typedTextBox6);
+
+			Point location7 = new Point(300, 200);
+			Size size7 = new Size(200, 100);
+			TypedTextBox typedTextBox7 = new TypedTextBox("label", location7, size7, action);
+			this.Controls.Add(typedTextBox7);
 		}
 	}
 }
